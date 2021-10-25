bba6625ae7fc164abe6c835baa1420a645116d3d
Trevor Ross <trevorross@Trevors-MacBook-Air.local>
2021-10-25 09:44:56 -0500

    10.18.2021

--- pyrs.py
+++ pyrs.py
@@ -4,4 +4,7 @@ import numpy as np
 pyrs = pd.read_csv('/Users/trevorross/Desktop/My Projects/bettingatwork/players_and_agents.csv')
 pyrs.head()
 
-pyrs.Agent.value_counts()
+pyrs[pyrs.Agent=="Orso"][pyrs.Name.str.contains("Chris")]
+
+dataFrame[dataFrame['column name'].str.contains('string')]
+
