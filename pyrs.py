65cebfd007d822a91b492b6f417e5b29905c9ca3
Trevor Ross <trossco84@gmail.com>
2022-09-26 09:23:52 -0400

    9.19.2022

--- pyrs.py
+++ pyrs.py
@@ -2,21 +2,8 @@ import pandas as pd
 import numpy as np
 from pandas.core.base import DataError
 
-player_data = pd.read_csv('/Users/trevorross/Desktop/My Projects/bettingatwork/players_and_agents.csv')
-player_data.head()
-
-player_data[player_data.Agent=="Orso"][player_data.Name.str.contains("Chris")]
-
-dataFrame[dataFrame['column name'].str.contains('string')]
-
-pyraccess = pd.read_clipboard()
-
-pa2 = pyraccess.drop_duplicates('Player',keep='last')
-pa2.to_clipboard()
-
 
 player_data = pd.read_csv('/Users/trevorross/Desktop/My Projects/bettingatwork/players_and_agents.csv')
-
 
 
 def retrieve_players(player_data):
